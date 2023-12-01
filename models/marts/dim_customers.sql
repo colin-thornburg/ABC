0c9636df4cde4b62b85b0f07e160c73eb0269ddb
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2023-12-01 17:23:54 +0000

    fixed dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -35,7 +35,7 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders_
 
     from orders
 
