cdc29efd30c785d8632420768ee2f455a60844ba
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-03-12 13:42:51 +0000

    made a fix

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -2,7 +2,7 @@
 
 select 
     id as customer_id,
-
+    first_name,
     last_name,
     {{ dbt_utils.generate_surrogate_key(['first_name', 'last_name']) }} as surr_col
 from {{ source('jaffle_shop', 'customers') }}
