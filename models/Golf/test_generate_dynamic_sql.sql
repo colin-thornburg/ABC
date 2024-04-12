20a6544e01ebb1f059cc88f736696af7d96c0153
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-04-12 02:38:04 +0000

    some dynamic SQL macros

--- models/Golf/test_generate_dynamic_sql.sql
+++ models/Golf/test_generate_dynamic_sql.sql
@@ -1,12 +1,5 @@
--- my_dynamic_model.sql
+-- models/select_users.sql
 
-{% set dynamic_sql = generate_simple_dynamic_sql('users', select_key_columns_only=true) %}
+{{ config(materialized='view') }}
 
--- Now, use the dynamic_sql as part of a larger query, or directly execute it
--- Example of using the result as a part of a larger query:
-WITH dynamic_query AS (
-    {{ dynamic_sql }}
-)
-
-SELECT *
-FROM dynamic_query
+{{ generate_select_sql('users') }}
