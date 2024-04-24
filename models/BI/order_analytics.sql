f230e6b02eaa7d7137ea232caa4f36a58620eb38
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-04-24 19:40:54 +0000

    changed contract test

--- models/BI/order_analytics.sql
+++ models/BI/order_analytics.sql
@@ -6,10 +6,10 @@ orders as (select * from {{ ref("fct_orders") }})
 select
     orders.order_id,
     orders.customer_id,
-    customers.first_name,
+    
     customers.last_name,
     orders.order_date,
-
+    orders.state as state,
     orders.payment_id,
     orders.payment_method,
     {{ format_dollars('amount') }} as amount,
