8159a21d23d6236e17763b3eb90cc7f7b16f1d55
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-07-01 10:32:20 +0000

    did some things

--- models/BI/order_analytics.sql
+++ models/BI/order_analytics.sql
@@ -4,17 +4,6 @@ customers as (select * from {{ ref("dim_customers") }}),
 orders as (select * from {{ ref("fct_orders") }})
 
 select
-    orders.order_id,
-    orders.customer_id,
-    customers.first_name,
-    customers.last_name,
-    orders.order_date,
-    orders.state as state,
-    orders.payment_id,
-    orders.payment_method,
-    {{ format_dollars('amount') }} as amount,
-    orders.created_at,
-    customers.first_order_date,
-    customers.most_recent_order_date
+    orders.order_id
 from orders
 left join customers on orders.customer_id = customers.customer_id
