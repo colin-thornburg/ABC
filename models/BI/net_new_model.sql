e34fad595347052342dc36fac41282f5c54a30f3
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-07-15 16:28:22 +0000

    Major Clean Up - removed everything unrelated to OR

--- models/BI/net_new_model.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
-
-
-Select * from {{ ref('order_analytics') }}
--- models/Golf/cast_columns_ex_model.sql
+++
@@ -1,7 +0,0 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
-
-{{ cast_product_columns('your_table_name_here') }}
