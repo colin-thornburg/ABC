1af4c5c737e3663911a5acd3500e575a61617a63
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-08-07 20:03:55 +0000

    sdg

--- models/Spartan/fct_item_dim.sql
+++ models/Spartan/fct_item_dim.sql
@@ -1,48 +1,26 @@
 {{ config(
     materialized='incremental',
-    unique_key=['ITEM_PK', 'START_DT'],
-    incremental_strategy='merge',
-    merge_update_columns = ['END_DT', 'CURRENT_FLG', 'LAST_UPDT_TMSP']
+    unique_key=['division_code', 'item_code', 'cdc_timestamp'],
+    strategy='append'
 ) }}
 
--- This model creates the final ITEM_DIM table, combining data from hub and satellite tables
-WITH latest_data AS (
+-- This model creates a type 2 table with a simple incremental model...
+WITH data AS (
     SELECT
-        hub.item_key AS ITEM_PK,
-        hub.division_code AS FACILITY_PK,
-        hub.item_code AS ITEM_NBR,
-        hub.department,
-        hub.item_group,
-        hub.subgroup,
-        hub.gpc_merchandise_class_code AS MDSE_CLASS_KEY,
-        sat.shelf_life,
-        sat.case_upc_code AS UPC_CASE,
-        sat.unit_upc_code AS upc_unit,
-        sat.code_date_flag,
-        sat.code_date_max,
-        sat.code_date_min,
-        sat.item_status,
-        sat.item_description AS ITEM_DESCRIP,
-        sat.brand,
-        sat.vendor_code,
-        sat.list_cost,
-        sat.suggested_retail_price AS ISI_CPR_SRP,
-        sat.master_pack,
-        sat.first_ship_date AS ISI_FIRST_SHIP_DATE,
-        sat.average_wholesale_price AS AVG_WHOLESALE_PRICE,
-        sat.shipper_type AS SHIPPER_FLAG,
-        sat.hide_on_insite_flag AS INSITE_FLG,
-        sat.effective_from AS START_DT,
-        sat.effective_to AS END_DT,
-        CASE WHEN sat.effective_to = '9999-12-31'::timestamp THEN 1 ELSE 0 END AS CURRENT_FLG,
-        hub.created_at AS CREATE_TMSP,
+        int.item_key AS ITEM_PK,
+        int.division_code AS FACILITY_PK,
+        int.item_code AS ITEM_NBR,
+        int.department,
+        int.item_group,
+        int.subgroup,
+        int.gpc_merchandise_class_code AS MDSE_CLASS_KEY,
         CURRENT_TIMESTAMP() AS LAST_UPDT_TMSP,
         'MDM' AS ORIGIN
-    FROM {{ ref('hub_item') }} hub
-    JOIN {{ ref('sat_item') }} sat ON hub.item_key = sat.item_key
+    FROM {{ ref('int_item_transformed') }} int
+    
     {% if is_incremental() %}
-    WHERE sat.effective_from > (SELECT MAX(START_DT) FROM {{ this }})
+    WHERE int.effective_from > (SELECT MAX(START_DT) FROM {{ this }})
     {% endif %}
 )
 -- Select all the latest data
-SELECT * FROM latest_data
+SELECT * FROM data
