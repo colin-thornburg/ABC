293f30526771bff76a7ba76b4760f67ce13bea1e
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-09-13 14:42:35 +0000

    created some data

---
+++ models/Sept-11-2024/target_simple_example.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized='incremental',
+    unique_key='policy_key',
+    on_schema_change='ignore',
+    incremental_strategy='merge',
+    merge_update_columns=[
+        'client_key',
+        'product_key',
+        'effective_date_key',
+        'expiration_date_key',
+        'written_premium_amt',
+        'annualized_premium_amt',
+        'commission_revenue_amt_usd',
+        'fee_revenue_amt_usd'
+    ]
+) }}
+
+SELECT 
+    policy_key,
+    client_key,
+    product_key,
+    effective_date_key,
+    expiration_date_key,
+    written_premium_amt,
+    annualized_premium_amt,
+    commission_revenue_amt_usd,
+    fee_revenue_amt_usd
+FROM {{ ref('temp_edw_f_policy_EPIC_US_FINAL') }}
+WHERE insert_to_fact
